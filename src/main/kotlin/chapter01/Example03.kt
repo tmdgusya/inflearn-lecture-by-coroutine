4f58da693652ca0f88adac935b6cb295f780726f
정승현 <roach@woowahan.com>
2023-02-02 18:58:40 +0900

    Asynchronous-Programming

---
+++ src/main/kotlin/chapter01/Example03.kt
@@ -0,0 +1,41 @@
+package chapter01
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+private fun shower() {
+    Thread.sleep(3000)
+    println("샤워 완료!")
+}
+
+private fun makeToast() {
+    val thread = Thread {
+        println("토스트 제작 스레드 : ${Thread.currentThread().name}")
+        Thread.sleep(3000)
+        println("토스트 제작 완료")
+    }
+    thread.start()
+}
+
+private fun eatToToast() {
+    Thread.sleep(2000)
+    println("토스트 먹기 완료!")
+}
+
+private fun workOut() {
+    Thread.sleep(10000)
+    println("운동 완료!")
+}
+
+@OptIn(ExperimentalTime::class)
+fun main() {
+    println("메인 스레드 : ${Thread.currentThread().name}")
+    val executedTime = measureTime {
+        shower()
+        makeToast()
+        eatToToast()
+        workOut()
+    }
+
+    println(executedTime)
+}
